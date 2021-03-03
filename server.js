a56fa470441a839071321166e29562135f929978
Ruju Bajracharya <ruju@kazistudios.com>
2021-03-03 17:44:45 +0545

    added peer server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid');
-const port = process.env.PORT || 3000;
+const { PeerServer } = require('peer');
+const peerServer = PeerServer({ port: 3001, path: '/' });
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -27,6 +28,7 @@ io.on('connection', socket => {
 	})
 })
 
-server.listen(port, function() {
-	console.log('server started successfully.')
-})
+
+
+const port = process.env.PORT || 3000;
+server.listen(port, () =>  console.log('server started on ' + port))
