bd0069d838594ec5f6c2606db560cdde910e166d
Ruju Bajracharya <ruju@kazistudios.com>
2021-03-03 17:57:50 +0545

    added dotenv file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const env = require('dotenv').config()
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid');
+
+const peerPort = process.env.PEERPORT || 3001
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 3001, path: '/' });
+const peerServer = PeerServer({ port: peerPort, path: '/' });
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
